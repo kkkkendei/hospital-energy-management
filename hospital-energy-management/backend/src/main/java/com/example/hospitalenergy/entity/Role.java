f75faea2873516b599fee61ef64087d2dfedfa50
kkkkendei <authoryu0607@gmail.com>
2025-06-04 09:14:28 +0800

    refactor: 优化

--- hospital-energy-management/backend/src/main/java/com/example/hospitalenergy/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.hospitalenergy.entity;
- 
-public enum Role {
-    ROLE_USER,  // 普通用户
-    ROLE_ADMIN  // 管理员
-} 
--- hospital-energy-management/backend/src/main/java/com/example/hospitalenergy/service/EnergyStatsService.java
+++
@@ -1,8 +0,0 @@
-package com.example.hospitalenergy.service;
-
-import com.example.hospitalenergy.dto.EnergyStatsResponse;
-import com.example.hospitalenergy.dto.QueryParametersDto;
-
-public interface EnergyStatsService {
-    EnergyStatsResponse getEnergyStatistics(QueryParametersDto params);
-} 
